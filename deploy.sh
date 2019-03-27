21f9a10cb8972e3a93dd843805babdc78378a3fb
liub1934 <liub1934@gmail.com>
2019-03-27 17:38:05 +0800

    deploy测试

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git config --local user.name "liub1934"
+git config --local user.email "liub1934@gmail.com"
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f https://${access_token}@github.com/liub1934/lb-element-table.git master:gh-pages
+
+cd -
